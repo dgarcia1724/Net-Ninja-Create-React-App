cefac774a66052a9f353ea8df68bc8618fdb47a1
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-10 12:15:12 -0800

    output list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState("Mario");
+  const [events, setEvents] = useState([
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser", id: 2 },
+    { title: "moo moo farm", id: 3 },
+  ]);
 
   const handleClick = () => {
     setName("luigi");
@@ -13,6 +18,13 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>
+            {index + 1} - {event.title}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
