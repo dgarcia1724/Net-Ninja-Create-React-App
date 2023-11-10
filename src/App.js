47d97170f02d37c662e2d963a476d09850a5122d
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-10 14:34:30 -0800

    starting point to practice deleting using filter

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("Mario");
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser", id: 2 },
     { title: "moo moo farm", id: 3 },
   ]);
 
-  const handleClick = () => {
-    setName("luigi");
-    console.log(name);
-  };
+  const handleClick = () => {};
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
       {events.map((event, index) => (
         <div key={event.id}>
           <h2>
             {index + 1} - {event.title}
           </h2>
+          <button onClick={handleClick}>Delete</button>
         </div>
       ))}
     </div>
