071317f20529558717643b790ca7de976925a7d9
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-11 00:10:05 -0800

    33. fresh start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-import React, { useState } from "react";
+import { useState } from "react";
 import Title from "./components/Title";
 import Modal from "./components/Modal";
+import EventList from "./components/EventList";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser", id: 2 },
     { title: "moo moo farm", id: 3 },
   ]);
+
+  console.log(showModal);
 
   const handleClick = (id) => {
     setEvents((events) => events.filter((event) => event.id !== id));
@@ -20,6 +24,14 @@ function App() {
   };
   const handleShowEvents = () => {
     setShowEvents(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleOpen = () => {
+    setShowModal(true);
   };
 
   const subtitle = "All the latest events in Marioland";
@@ -39,29 +51,21 @@ function App() {
           <button onClick={handleShowEvents}>show events</button>
         </div>
       )}
-      {showEvents &&
-        events.map((event, index) => (
-          <React.Fragment key={event.id}>
-            <h2>
-              {index + 1} - {event.title}
-            </h2>
-            <button onClick={() => handleClick(event.id)}>Delete event</button>
-          </React.Fragment>
-        ))}
-      {/* <Modal>
-        <h2>10% Off Coupon Code!!</h2>
-        <p>Use the code NINJA10 at the checkout.</p>
-      </Modal> */}
-      <Modal>
-        <h2>Terms and Conditions</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi labore
-          molestias ea delectus atque dolores consequuntur mollitia corporis
-          assumenda quod distinctio maiores tenetur, id architecto adipisci fuga
-          esse ut doloremque!
-        </p>
-        <a href="#">find out more...</a>
-      </Modal>
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
+      {showModal && (
+        <Modal handleClose={handleClose}>
+          <h2>Terms and Conditions</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+            labore molestias ea delectus atque dolores consequuntur mollitia
+            corporis assumenda quod distinctio maiores tenetur, id architecto
+            adipisci fuga esse ut doloremque!
+          </p>
+        </Modal>
+      )}
+      <div>
+        <button onClick={handleOpen}>Open modal</button>
+      </div>
     </div>
   );
 }
