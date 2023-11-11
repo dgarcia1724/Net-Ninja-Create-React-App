5b10db60b502d9f054b77f65ae2d7e68f9558324
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-11 13:55:29 -0800

    38. fresh start

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,17 @@
 import ReactDOM from "react-dom";
 import "./Modal.css";
 
-function Modal({ children, handleClose }) {
+function Modal({ children, handleClose, isSalesModal }) {
   return ReactDOM.createPortal(
     <div className="modal-backdrop">
-      <div className="modal">
+      <div
+        className="modal"
+        style={{
+          border: "4px solid",
+          borderColor: isSalesModal ? "#ff4500" : "#555",
+          textAlign: "center",
+        }}
+      >
         {children}
         <button onClick={handleClose}>close</button>
       </div>
