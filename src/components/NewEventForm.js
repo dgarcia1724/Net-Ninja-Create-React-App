8082dcf45c0d305d2a02b4716bdc931a37070587
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-11 19:24:32 -0800

    43. fresh start

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -5,24 +5,34 @@ function NewEventForm() {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
 
-  // const handleChange = (e) => {
-  //   setTitle(e.target.value);
-  // };
+  const resetForm = () => {
+    setTitle("");
+    setDate("");
+  };
 
   return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={(e) => setDate(e.target.value)} />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
       </label>
       <button>Submit</button>
       <p>
         title - {title}, date - {date}
       </p>
+      <p onClick={resetForm}>reset the form</p>
     </form>
   );
 }
