850a246ebf9bd43993e0944f87b51e8d413f64af
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-12 19:48:41 -0800

    useRef

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,21 +1,24 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./NewEventForm.css";
 
 function NewEventForm({ addEvent }) {
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [date, setDate] = useState("");
+  const title = useRef();
+  const date = useRef();
 
   const resetForm = () => {
-    setTitle("");
-    setDate("");
+    title.current.value = "";
+    date.current.value = "";
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(title, date);
 
     const event = {
-      title: title,
-      date: date,
+      title: title.current.value,
+      date: date.current.value,
       id: Math.floor(Math.random() * 10000),
     };
     addEvent(event);
@@ -26,19 +29,11 @@ function NewEventForm({ addEvent }) {
     <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+        <input type="text" ref={title} />
       </label>
       <label>
         <span>Event Date:</span>
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
+        <input type="date" ref={date} />
       </label>
       <button>Submit</button>
     </form>
